d6a472aed5e8d6a0b17b77ebf70cf3b33081ad5c
lildworks <lildworks@gmail.com>
2020-03-12 19:47:06 +0100

    FEAT: Printing all validation messages (in JSON format too)

--- src/main/java/com/lildutils/springboot/validation/messages/LDuValidationMessages.java
+++ src/main/java/com/lildutils/springboot/validation/messages/LDuValidationMessages.java
@@ -1,5 +1,8 @@
 package com.lildutils.springboot.validation.messages;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 public interface LDuValidationMessages
 {
 	String	VM_NOT_NULL				= "VM_NOT_NULL";
@@ -9,4 +12,52 @@ public interface LDuValidationMessages
 	String	VM_INVALID_LENGTH_MIN	= "VM_INVALID_LENGTH_MIN";
 	String	VM_INVALID_LENGTH_MAX	= "VM_INVALID_LENGTH_MAX";
 	String	VM_INVALID_SIZE			= "VM_INVALID_SIZE";
+
+	static void printAll()
+	{
+		Arrays.asList( LDuValidationMessages.class.getDeclaredFields() ).forEach( f -> {
+			try
+			{
+				System.out.println( LDuValidationMessages.class.getField( f.getName() ).get( f.getName() ) );
+			}
+			catch( Exception e )
+			{
+				e.printStackTrace();
+			}
+		} );
+	}
+
+	static void printAllAsJSON()
+	{
+		System.out.println( "{" );
+		Field[] fields = LDuValidationMessages.class.getDeclaredFields();
+		int l = fields.length;
+		for( int i = 0; i < l; i++ )
+		{
+			try
+			{
+				final String field = LDuValidationMessages.class.getField( fields[i].getName() )
+						.get( fields[i].getName() ).toString();
+				if( i < (l - 1) )
+				{
+					System.out.println( "\"" + field + "\": \"" + field + "\"," );
+				}
+				else
+				{
+					System.out.println( "\"" + field + "\": \"" + field + "\"" );
+				}
+			}
+			catch( Exception e )
+			{
+				e.printStackTrace();
+			}
+		}
+		System.out.println( "}" );
+	}
+
+	static void main( String[] args )
+	{
+//		LDuValidationMessages.printAll();
+		LDuValidationMessages.printAllAsJSON();
+	}
 }
